3ac359324ba683e661ea4546e074c5376a038482
Khadidja IKHLEF <khadidja.ikhlef.mobile@gmail.com>
2021-04-30 19:39:13 +0200

    version 1 qui fonctionne

--- src/controllers/rocket.controller.js
+++ src/controllers/rocket.controller.js
@@ -1,19 +1,17 @@
 const rocket = require('../models').rocket;
+//const db = require('../models');
 
 module.exports = {
-
+// Afficher toutes les fusées
     async getAllRockets(requete, reponse) {
+        //const id = requete.params.id;
         try{
-            const {rocketId} = requete.params; // const rocketId = requete.params.rocketId;
+            //const {rocketId} = requete.params; // const rocketId = requete.params.rocketId;
+            const rocketCollection = await rocket.findOne();
 
-            const rocketCollection = await rocket.findOne({
-                id: requete.params.rocketId
-            });
             if(rocketCollection) {
 
-                const rocketCollection = await rocket.findAll({
-                    //where: { rocketId: rocketId }
-                })
+                const rocketCollection = await rocket.findAll()
 
                 reponse.status(201).send(rocketCollection);
             }
@@ -26,48 +24,63 @@ module.exports = {
             reponse.status(500).send(erreur);
         }
     },
-
+// Afficher la fusée à l'id = id
     async getOneRocket(requete, reponse) {
-        var id = requete.params.rocketId;
-        const {rocketId} = requete.params;
-        
-            const rocketCollection = await rocket.findByPk(id);
-
-            if (rocketCollection != null) {
-
+        //const {rocketId} = requete.params;
+        const id = requete.params.id;
+        try{
+            const Rocket = await rocket.findByPk(id);
+            if(Rocket) {
                 const Rocket = await rocket.findByPk(id);
-
-                if (Rocket) {
-                    reponse.status(201).send(rocket);
-                }
-                else {
-                    reponse.status(404).send("Rocket Not Found");
-                }
+                
+                reponse.status(201).send(Rocket);
             }
             else {
-                reponse.status(404).send("Rocket Not Found")
+                reponse.status(404).send("Rocket not Found !!!")
             }
-        
+        }
+        catch(erreur){
+            console.log(erreur);
+            reponse.status(500).send(erreur);
+        }
+
+        // try {
+        //     //const Rocket = await rocket.findByPk((id));
+        //      const Rocket = await rocket.findOne(({
+        //          where: { id: id }
+        //     }));
+
+        //     if (Rocket) {
+        //         reponse.status(201).send(rocket);
+        //         console.log(reponse);
+        //     }
+        //     else {
+        //         reponse.status(404).send("Rocket Not Found");
+        //     }
+        // }
+        // catch (erreur) {
+        //     console.log(erreur);
+        //     reponse.status(400).send(erreur);
+        // }
 
     },
-
+// Créer une fusée
     async createRocket(requete, reponse) {
-
+        //const id = requete.params.id;
         try{
-            const rocketCollection = await rocket.findOne({
-                id: requete.params.rocketId
-            });
-            if(rocketCollection){
-                const rocketCreated = await rocket.create({
+            //const Rocket = await rocket.findOne({   id: id  })
+            const Rocket = await rocket.findOne()
+                if(Rocket){
+                const Rocket = await rocket.create({
                     nom: requete.body.nom,
                     fabrication: requete.body.fabrication,
                     organisation: requete.body.organisation,
                     destination: requete.body.destination
                 })
-                reponse.status(201).send(rocketCreated)
+                reponse.status(201).send(Rocket)
             }
             else{
-                resizeBy.status(404).send("Rocket Not Found")
+                reponse.status(404).send("Rocket Not Found")
             }
         }
         catch(erreur){
@@ -75,7 +88,64 @@ module.exports = {
             reponse.status(400).send(erreur);
         }
     },
+// Mettre à jour une fusée
+    async updateRocket(requete, reponse) {
+        //const {rocketId} = req.params; // ou const rocketId = req.params.rocketId; en ES5
+        const id = requete.params.id;
+        try {
+            const Rocket = await rocket.findByPk(id);
+                if (Rocket) {
+                    const updatedRocket = await Rocket.update(requete.body, { where: {  id: id  } })
+                    // const updatedRocket = await Rocket.update({
+                    //     nom: requete.body.nom,
+                    //     fabrication: requete.params.fabrication,
+                    //     organisation: requete.params.organisation,
+                    //     destination: requete.params.destination
+                    // })
 
+                    reponse.status(201).send(updatedRocket);
+                }
+                else {
+                    reponse.status(404).send("Rocket Not Found");
+                }
+        }
+        catch (erreur) {
+            console.log(erreur);
+            reponse.status(400).send(erreur);
+        }
 
+    },
+// Supprimer une fusée
+    async deleteRocket(requete, reponse) {
+
+        // const {rocketId} = requete.params; 
+        const id = requete.params.id;
+        try {
+            const Rocket = await rocket.findByPk(id);
+
+            if (Rocket) {
+
+                const deletedRocket = await rocket.findOne({
+                    where: { id: id } // condition importante
+                })
+
+                if (deletedRocket) {
+                    deletedRocket.destroy();
+                    reponse.status(201).send("Deleted Rocket");
+                }
+                else {
+                    reponse.status(404).send("Rocket Not Found");
+                }
+            }
+            else {
+                requete.status(404).send("Rocket Not Found")
+            }
+        }
+        catch (erreur) {
+            console.log(erreur);
+            reponse.status(400).send(erreur);
+        }
+
+    }
 
 }
--- src/routes/rocket.routes.js
+++ src/routes/rocket.routes.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
     app.get("/api/rockets", controller.getAllRockets); // http://localhost:8080/api/rockets
     app.get("/api/rockets/:id", controller.getOneRocket); // http://localhost:8080/api/rockets/1
     app.post("/api/rockets", controller.createRocket); // http://localhost:8080/api/rockets
-    //app.put("/api/rockets/:id", controller.updatePostByUser); // http://localhost:8080/api/rockets/1
-    //app.delete("/api/rockets/:id", controller.deletePostByUser); // http://localhost:8080/api/rockets/1
+    app.put("/api/rockets/:id", controller.updateRocket); // http://localhost:8080/api/rockets/1
+    app.delete("/api/rockets/:id", controller.deleteRocket); // http://localhost:8080/api/rockets/1
 
 }
